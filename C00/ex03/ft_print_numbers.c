00e61a7701879c940f94763974c5164fb49785a6
Hakan Yargıcı <hakanyargici@outlook.com>
2024-02-01 13:10:17 +0300

    C00 içerisinde ilerleme kaydedildi.

--- C00/ex03/ft_print_numbers.c
+++ C00/ex03/ft_print_numbers.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
+
+void    ft_print_numbers(void)
+{
+        int start;
+        
+        start = '0';
+        while(start <= '9')
+        {
+                write(1, &start, 1);
+                start++;
+                
+        }
+}
+
+int    main(){
+    ft_print_numbers();
+    return(0);
+}
