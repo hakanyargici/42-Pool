2dcb701bea1a0dad8ee9853d74e4e92da742dbea
Hakan Yargıcı <hakanyargici@Hakan.local>
2024-02-15 08:38:49 +0300

    c04, c05, c06 dosyaları eklendi.

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,24 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	s;
+	int	result;
+
+	i = 0;
+	s = 1;
+	result = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			s = s * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (str[i] - 48) + (result * 10);
+		i++;
+	}
+	return (result * s);
+}
