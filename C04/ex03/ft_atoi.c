11ad46e9c4c9edbe0e0af73bda90a14c8739e4e9
Yusuf Hakan Yargici <yyargici@k1m17s07.42istanbul.com.tr>
2024-02-15 16:07:36 +0300

    Headerler ile beraber her şey eksiksiz verildi.

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yyargici <yyargici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 12:45:21 by yyargici          #+#    #+#             */
+/*   Updated: 2024/02/15 13:42:10 by yyargici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_atoi(char *str)
 {
 	int	i;
-	int	s;
+	int	sign;
 	int	result;
 
 	i = 0;
-	s = 1;
+	sign = 1;
 	result = 0;
 	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
 		i++;
 	while (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			s = s * -1;
+			sign = sign * -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
@@ -20,5 +32,5 @@ int	ft_atoi(char *str)
 		result = (str[i] - 48) + (result * 10);
 		i++;
 	}
-	return (result * s);
+	return (result * sign);
 }
